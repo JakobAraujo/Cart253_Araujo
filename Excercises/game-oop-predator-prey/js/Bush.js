494c5ef7c6f781e84457eb388b21024d07f1f268
JakobAraujo <55066442+JakobAraujo@users.noreply.github.com>
2019-10-15 17:16:54 -0400

    P2:Started Bush class

---
+++ Excercises/game-oop-predator-prey/js/Bush.js
@@ -0,0 +1,12 @@
+//Bush
+//
+// A class that represents a bush
+// Randomly generated around the canvass.
+// if the Predator is standing inside of it
+// they stop losing health.
+
+class Bush{
+  constructor() {
+    this.occupied = false; //This marks whether or not the predator is within the boundries of the bush.
+  }
+}
