3c605bcb47ca16d7662381f39d2bb3deec71ce66
JakobAraujo <55066442+JakobAraujo@users.noreply.github.com>
2019-10-22 23:05:33 -0400

    E5: Added second player.

--- Excercises/Excercise 5/game-oop-predator-prey/js/script.js
+++ Excercises/Excercise 5/game-oop-predator-prey/js/script.js
@@ -7,7 +7,7 @@
 
 // Our predator
 let tiger;
-
+let wolf;
 // The three prey
 let antelope;
 let zebra;
@@ -19,6 +19,7 @@ let bee;
 // Creates objects for the predator and three prey
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  wolf = new PredatorII(100, 100, 5, color(50, 255, 0), 40);
   tiger = new Predator(100, 100, 5, color(200, 200, 0), 40);
   antelope = new Prey(100, 100, 10, color(255, 100, 10), 50);
   zebra = new Prey(100, 100, 8, color(255, 255, 255), 60);
@@ -34,19 +35,25 @@ function draw() {
 
   // Handle input for the tiger
   tiger.handleInput();
-
+  wolf.handleInput();
   // Move all the "animals"
+  wolf.move();
   tiger.move();
   antelope.move();
   zebra.move();
   bee.move();
 
   // Handle the tiger eating any of the prey
+  wolf.handleEating(antelope);
+  wolf.handleEating(zebra);
+  wolf.handleEating(bee);
+
   tiger.handleEating(antelope);
   tiger.handleEating(zebra);
   tiger.handleEating(bee);
 
   // Display all the "animals"
+  wolf.display();
   tiger.display();
   antelope.display();
   zebra.display();
